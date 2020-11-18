70ae0add2b2d7089ebe1fbee7fb618727e841e7a
Dayane Gabriela Santos Cordeiro <1209930@sga.pucminas.br>
2020-11-18 18:09:14 -0300

    [General] Enhancement: Front end

--- app_web_papi/lib/views/pessoa_list.view.dart
+++ app_web_papi/lib/views/pessoa_list.view.dart
@@ -12,63 +12,83 @@ class PessoaListView extends StatefulWidget {
 }
 
 class _PessoaListViewState extends State<PessoaListView> {
-
   @override
   Widget build(BuildContext context) {
-
     final _controller = Provider.of<PessoaController>(context);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("PUC APP"),
+        title: Text("Dexter Pizzaria 🍕"),
         centerTitle: true,
+        backgroundColor: Colors.cyan[800],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           _showPessoaFormView();
         },
         child: Icon(Icons.add),
       ),
       body: RefreshIndicator(
-          onRefresh: () async {
-            await _controller.getAll();
-          },
-          child: Container(
-            child: Observer(builder: (_) {
-              if (_controller.status == AppStatus.loading) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else if (_controller.status == AppStatus.success) {
-                return ListView.builder(
-                    padding: const EdgeInsets.all(8),
-                    itemCount: _controller.list.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(
-                        title: Text(_controller.list[index].nome),
+        onRefresh: () async {
+          await _controller.getAll();
+        },
+        child: Container(
+          child: Observer(builder: (_) {
+            if (_controller.status == AppStatus.loading) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (_controller.status == AppStatus.success) {
+              return ListView.builder(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: _controller.list.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      color: Colors.grey[600],
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.account_circle,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                        title: Text(
+                          _controller.list[index].nome,
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                        subtitle: Text(
+                          _controller.list[index].localidade,
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
                         onTap: () {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => PessoaFormView(pessoa: _controller.list[index]),
+                              builder: (context) => PessoaFormView(
+                                  pessoa: _controller.list[index]),
                             ),
                           );
                         },
-                      );
-                    }
-                );
-              } else {
-                return Text("${_controller.status.value}");
-              }
-            }),
-          ),
+                      ),
+                    );
+                  });
+            } else {
+              return Text("${_controller.status.value}");
+            }
+          }),
         ),
+      ),
+      backgroundColor: Colors.grey[800],
     );
   }
 
   void _showPessoaFormView({Pessoa pessoa}) async {
-    await Navigator.push(context,
-        MaterialPageRoute(builder: (context) => PessoaFormView(pessoa: pessoa,))
-    );
+    await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => PessoaFormView(
+                  pessoa: pessoa,
+                )));
   }
 }
